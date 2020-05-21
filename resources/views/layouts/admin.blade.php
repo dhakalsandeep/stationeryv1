d3e8702c3049946bb029940b5641e8bc5a4f22b6
Sandeep <dhakalsandeep38@gmail.com>
2020-05-21 08:51:19 +0545

    initial commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
     <header class="app-header navbar">
         <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
             <span class="navbar-toggler-icon"></span>
-        </button>
+        </button>app-header
         <a class="navbar-brand" href="#">
             <span class="navbar-brand-full">Project</span>
             <span class="navbar-brand-minimized">P</span>
